1dc0184e7daa269febb8e9d501f4691e8a6c8ee1
BeregszasziLevente <beregszaszi.levente.2024d@telekimezotur.hu>
2026-09-17 12:37:48 +0200

    2. feladat

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Az utas neve: ");
+string nev=Console.ReadLine();
+
+Console.Write("Megtett távolság km-ben: ");
+int tav = int.Parse(Console.ReadLine());
+
+Console.Write("Éjszakai utazás? (igen/nem) ");
+string estiInput=Console.ReadLine();
+bool esti = false;
+if (estiInput=="igen") esti= true;
+
+Console.Write("Alapdíj: ");
+int alapdij=int.Parse(Console.ReadLine());
+
+Console.Write("Kilóméterdíj: ");
+int kmdij=int.Parse(Console.ReadLine());
+
+double potlek = 1.0;
+if (esti) potlek = 1.2;
+
+int alaposszeg = alapdij + (kmdij * kmdij);
+
